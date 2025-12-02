cc790aaa7c59e3b077c99e50c2fb68fa69a8ea23
jessicatinguely <jessicatinguely@icloud.com>
2025-12-02 10:19:46 -0300

    :rewind: Retornando arquivo App.tsx para o estado original

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,33 @@
-import { useState } from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Footer from "./components/footer/Footer"
-import Navbar from "./components/navbar/Navbar"
-import Home from "./pages/home/Home"
-
-type MenuState = 'closed' | 'open';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-
-  const [menuState, setMenuState] = useState<MenuState>('closed');
-  const toggleMenu = (): void => {
-    setMenuState(prevState => prevState === 'closed' ? 'open' : 'closed');
-  };
-  const closeMenu = (): void => {
-    setMenuState('closed');
-  };
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <BrowserRouter>
-        
-        <Navbar 
-          menuState={menuState}
-          onMenuToggle={toggleMenu}
-          onMenuClose={closeMenu}
-        />
-        
-        <div className='min-h-[90vh] w-full pt-16 bg-slate-100'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
